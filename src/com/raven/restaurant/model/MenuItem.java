c62e905b30fcb6231e3687e9c5dae5f60eee6ba1
Reshma <reshmakousar64@gmail.com>
2018-05-29 18:50:49 -0500

    second commit

--- src/com/raven/restaurant/model/MenuItem.java
+++ src/com/raven/restaurant/model/MenuItem.java
@@ -1,5 +1,32 @@
 package com.raven.restaurant.model;
 
 public class MenuItem {
-
+	private String menu_item;
+	private String name;
+	private String dish;
+	public MenuItem(String menu_item, String name, String dish) {
+		super();
+		this.menu_item = menu_item;
+		this.name = name;
+		this.dish = dish;
+	}
+	public String getMenu_item() {
+		return menu_item;
+	}
+	public void setMenu_item(String menu_item) {
+		this.menu_item = menu_item;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDish() {
+		return dish;
+	}
+	public void setDish(String dish) {
+		this.dish = dish;
+	}
+	
 }
--- src/com/raven/restaurant/model/Restaurant.java
+++ src/com/raven/restaurant/model/Restaurant.java
@@ -1,5 +1,40 @@
 package com.raven.restaurant.model;
 
 public class Restaurant {
-
+	private String res_id;
+	private String name;
+	private String location;
+	private String email_id;
+	public Restaurant(String res_id, String name, String location, String email_id) {
+		super();
+		this.res_id = res_id;
+		this.name = name;
+		this.location = location;
+		this.email_id = email_id;
+	}
+	public String getRes_id() {
+		return res_id;
+	}
+	public void setRes_id(String res_id) {
+		this.res_id = res_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getEmail_id() {
+		return email_id;
+	}
+	public void setEmail_id(String email_id) {
+		this.email_id = email_id;
+	}
+	
 }
